73d7256e25979b234d9cd3c54bdfd9393d4cd3fb
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-07-16 22:58:25 -0300

    Formulário de atualização

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -28,5 +28,9 @@ module.exports = {
         ]
 
         return db.query(query, values)
+    },
+
+    find(id) {
+        return db.query('SELECT * FROM products WHERE id = $1', [id])
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ routes.get('/', function(req, res) {
 });
 
 routes.get('/products/create', ProductController.create);
+routes.get('/products/:id/edit', ProductController.edit);
 routes.post('/products', ProductController.post);
 
 //atalho
